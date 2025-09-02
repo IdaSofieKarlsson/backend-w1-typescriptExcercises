5a7e3cb32762ae2bacf0e7827b74838ff216a9cc
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-09-02 15:49:14 +0200

    exercise 1-2 done

--- exercise1-2.ts
+++ exercise1-2.ts
@@ -2,3 +2,11 @@
  //Write a function eatFruit that prints "You ate an ...".
  //Test with "apple" and "orange".
 
+type Fruit = 'apple' | 'banana' | 'orange';
+
+const eatFruit = (fruit: Fruit) => {
+    console.log(`You ate an ${fruit}.`);
+};
+
+eatFruit('apple');
+eatFruit('orange');
